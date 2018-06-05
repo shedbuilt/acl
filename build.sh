89982c5e6cb2b8dbca260118ccd0d3214ddd1b84
Auston Stewart <auston@shedbuilt.net>
2018-06-04 16:11:41 -1000

    Add a docs option

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 # Patch
 sed -i -e 's|/@pkg_name@|&-@pkg_version@|' include/builddefs.in &&
 sed -i "s:| sed.*::g" test/{sbits-restore,cp,misc}.test &&
@@ -6,6 +7,7 @@ sed -i 's/{(/\\{(/' test/run &&
 sed -i -e "/TABS-1;/a if (x > (TABS-1)) x = (TABS-1);" libacl/__acl_to_any_text.c &&
 # Configure
 ./configure --prefix=/usr \
+            --docdir="$SHED_PKG_DOCS_INSTALL_DIR" \
             --disable-static \
             --libexecdir=/usr/lib &&
 # Build and Install
@@ -16,3 +18,7 @@ chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libacl.so" &&
 mkdir -v "${SHED_FAKE_ROOT}/lib" &&
 mv -v "${SHED_FAKE_ROOT}"/usr/lib/libacl.so.* "${SHED_FAKE_ROOT}/lib" &&
 ln -sfv ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/libacl.so") "${SHED_FAKE_ROOT}/usr/lib/libacl.so"
+# Prune Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
